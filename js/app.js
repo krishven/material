1ed7e51481042245a9c44e15bd8b8684adb2296f
krishven <venkrish007@gmail.com>
2016-07-14 15:16:13 -0700

    charts added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('altavault', []);
 
+/*app.config(function ($httpProvider) {
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+});*/
+
 /*app.factory('authInterceptor', function ($rootScope, $q, $window) {
 	return {
 		request: function (config) {
@@ -34,25 +41,112 @@ app.controller('login', function($scope, $window, $http) {
 		$scope.loading = true;
 		//$window.sessionStorage.token = '752aa6bd-4702-43a3-85b9-00fff9c6a51b';
 		$window.alert('Hello');
-		$window.location.href = 'index.html';
+		//$window.location.href = 'index.html';
 
-		/*$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('altavault' + ':' + 'AVS3cr3t');
-		var xsrf = $.param({username: 'venkat', password: 'bowbow', grant_type:'password'});
+		$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('altavault' + ':' + 'AVS3cr3t');
+		var xsrf = $.param({username: 'siva', password: 'saycheese', grant_type:'password'});
 		$http({
-				url: 'https://10.192.5.98:8443/capi/v1/auth/login',
+				url: 'https://172.16.33.180:8443/capi/v1/auth/login',
 				method: 'POST',
 				data: xsrf,
 				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 		}).success(function (data, status, headers, config) {
 					//handle success
-					console.log("Success");
+					$window.alert("Success");
 					console.log(data);
 		}).error(function (data, status, headers, config) {
 					//handle error
-					console.log("Error");
+					$window.alert("Error");
 					console.log(data);
-		});*/
+		});
 	};
+});
+
+app.controller('charts', function($scope, $window, $http) {
+	$scope.split = function(resp) {
+		var x = [];
+		var y = [];
+		for(var index = 0; index < resp[0].datapoints.length; index++) {
+			if(resp[0].datapoints[index][0] != null) {
+				x.push(moment.unix(resp[0].datapoints[index][1]).format('LT'));
+				y.push(resp[0].datapoints[index][0]);
+			}
+		}
+		console.log(x);
+		console.log(y);
+		return [x, y];
+		//data = JSON.stringify(data, null, '\t');
+		//return data;
+	}
+
+	$http.get("http://ripple.gdl.englab.netapp.com:1789/render/?target=012345678912.sar.q.ldavg-1&from=-1h&to=-1h&format=json")
+    .then(function(response) {
+        var resp = response.data;
+        var data = $scope.split(resp);
+
+        //SIMPLE LINE CHART
+		new Chartist.Line('#chart1', {
+		  labels: data[0],
+		  series: [
+		    data[1]
+		  ]
+		}, {
+		  fullWidth: true,
+		  chartPadding: {
+		    right: 40
+		  }
+		});
+    });
+
+    $http.get("http://ripple.gdl.englab.netapp.com:1789/render/?target=012345678912.sar.q.ldavg-5&from=-1h&format=json")
+    .then(function(response) {
+        var resp = response.data;
+        var data = $scope.split(resp);
+
+        //SIMPLE LINE CHART
+		new Chartist.Line('#chart2', {
+		  labels: data[0],
+		  series: [
+		    data[1]
+		  ]
+		}, {
+		  fullWidth: true,
+		  chartPadding: {
+		    right: 40
+		  }
+		});
+    });
+
+    $http.get("http://ripple.gdl.englab.netapp.com:1789/render/?target=scale(sumSeries(012345678912.sar.d.*.wr_sec.rate),512)&from=-1h&format=json")
+    .then(function(response) {
+        var resp = response.data;
+        var data = $scope.split(resp);
+
+        //SIMPLE LINE CHART
+		new Chartist.Line('#chart3', {
+		  labels: data[0],
+		  series: [
+		    data[1]
+		  ]
+		}, {
+		  fullWidth: true,
+		  chartPadding: {
+		    right: 40
+		  }
+		});
+    });
+
+	/*$http.get("http://ripple.gdl.englab.netapp.com:1789/render/?target=012345678912.rfs.megastore.io_reqs.rate&from=-4h&format=json")
+    .then(function(response) {
+        var resp = response.data;
+
+        // Create a new JavaScript Date object based on the timestamp
+		// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+		var date = moment.unix(resp[0].datapoints[10][1]).format('LT');
+
+        console.log(date);
+        console.log(JSON.stringify(resp[0].datapoints[0]));
+    });*/
 });
 
 app.controller('cards', function($scope, $window, $http) {
